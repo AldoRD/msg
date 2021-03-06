d402a4dfec0aa787fd082bef9f3f3644cb15c5a3
AldoRD <73556983+AldoRD@users.noreply.github.com>
2021-03-06 17:12:26 -0600

    Add files via upload

--- iframe.js
+++ iframe.js
@@ -2,7 +2,7 @@ function iniciar() {
   window.addEventListener("message", recibir);
 }
 function recibir(evento) {
-  var cajadatos = document.getElementById("cajadatos");
+  var cajadatos = document.getElementById("cajadatos2");
   cajadatos.innerHTML = "Mensaje desde: " + evento.origin + "<br>";
   cajadatos.innerHTML += "mensaje: " + evento.data;
 }
